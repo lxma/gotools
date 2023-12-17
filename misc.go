92c49975fdedc0cf42946d47125f9c9ace5eb0bf
lxma <lxma@sotalia.de>
2023-12-17 17:48:09 +0100

    Correct Partial naming

--- misc.go
+++ misc.go
@@ -17,54 +17,66 @@ func GetIntegersInString(line string) []int {
 	return result
 }
 
-// Partial takes a function with two arguments and one (the second) argument.
-// It returns a function with only one argument. Example
-//
-//	func add(a int, b int) int { return a + b }
-//	addTwo := Partial(add, 2)
-//
-// now, addTwo(3) returns 5
-// This is handy for mapping, e.g.:
-//
-//	Map([]int{1, 2, 3}, Partial(add,10))
-//
-// returns
-//
-//	[]int{11, 12, 13}
-func Partial[A any, B any, ResultType any](f func(A, B) ResultType, b B) func(A) ResultType {
-	return func(a A) ResultType {
-		return f(a, b)
-	}
-}
-
-// PartialR takes a function with two arguments and one (the first) argument.
-// It returns a function with only one argument. Example
+// Partial (similar to [PartialL] takes a function with two arguments and one (the first)
+// argument. It returns a function with only one argument. Example
 //
 //	func subtract(a int, b int) int { return a - b }
-//	subtractFromTen := Partial(add, 10)
+//	subtractFromTen := Partial(subtract, 10)
 //
-// now, subtractFromTen(3) returns 7
-// This is handy for mapping, e.g.:
+// Now, subtractFromTen(3) returns 7. This is handy for mapping, e.g.:
 //
 //	Map([]int{1, 2, 3}, Partial(subtract, 10))
 //
 // returns
 //
 //	[]int{9, 8, 7}
-func PartialR[A any, B any, ResultType any](f func(A, B) ResultType, a A) func(B) ResultType {
+func Partial[A any, B any, ResultType any](f func(A, B) ResultType, a A) func(B) ResultType {
 	return func(b B) ResultType {
 		return f(a, b)
 	}
 }
 
-// Partial2 takes a function with three arguments and two arguments.
-// It returns a function with only one argument. Example
+// PartialL (similar to [Partial] takes a function with two arguments and one (the second)
+// argument. It returns a function with only one argument. Example
 //
-//	func add(a int, b int, c int) int { return a + b + c }
-//	addTwo := Partial(add, 1, 1)
+//	func add(a int, b int) int { return a + b }
+//	addTwo := PartialL(add, 2)
 //
 // now, addTwo(3) returns 5
-func Partial2[A any, B any, C any, ResultType any](f func(A, B, C) ResultType, b B, c C) func(A) ResultType {
+// This is handy for mapping, e.g.:
+//
+//	Map([]int{1, 2, 3}, PartialL(add,10))
+//
+// returns
+//
+//	[]int{11, 12, 13}
+func PartialL[A any, B any, ResultType any](f func(A, B) ResultType, b B) func(A) ResultType {
+	return func(a A) ResultType {
+		return f(a, b)
+	}
+}
+
+// Partial2 (similar to [Partial2L] takes a function with three arguments and the first two arguments.
+// It returns a function with only one argument. Example
+//
+//	func subtract2(a int, b int, c int) int { return a - b - c }
+//	f := Partial2(subtract2, 10, 2)
+//
+// now, f(3) returns 5
+func Partial2[A any, B any, C any, ResultType any](f func(A, B, C) ResultType, a A, b B) func(C) ResultType {
+	return func(c C) ResultType {
+		return f(a, b, c)
+	}
+}
+
+// Partial2L (similar to [Partial2] takes a function with three arguments and the last two arguments.
+// It returns a function with only one argument. Example
+//
+//	func subtract2(a int, b int, c int) int { return a - b - c }
+//	subtractThree := Partial2L(subtract2, 1, 2)
+//
+// now, subtractThree(10) returns 7
+func Partial2L[A any, B any, C any, ResultType any](f func(A, B, C) ResultType, b B, c C) func(A) ResultType {
 	return func(a A) ResultType {
 		return f(a, b, c)
 	}
--- misc_test.go
+++ misc_test.go
@@ -10,10 +10,13 @@ func TestGetIntegersInString(t *testing.T) {
 }
 
 func TestPartial(t *testing.T) {
-	f := Partial(func(a int, b int) int { return a / b }, 2)
-	assert.Equal(t, 2, f(5))
-	g := PartialR(func(a int, b int) int { return a / b }, 10)
-	assert.Equal(t, 3, g(3))
-	h := Partial2(func(a int, b int, c int) int { return a + b + c }, 2, 3)
-	assert.Equal(t, 9, h(4))
+	f1 := Partial(func(a int, b int) int { return a / b }, 10)
+	assert.Equal(t, 3, f1(3))
+	f2 := PartialL(func(a int, b int) int { return a / b }, 2)
+	assert.Equal(t, 2, f2(5))
+	f3 := Partial2(func(a int, b int, c int) int { return a - b - c }, 10, 3)
+	assert.Equal(t, 5, f3(2))
+	f4 := Partial2L(func(a int, b int, c int) int { return a - b - c }, 2, 3)
+	assert.Equal(t, 5, f4(10))
+
 }
